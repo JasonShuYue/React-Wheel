a10210a59a5d71db58e06aaf2b0ccd2ee39f3363
Jason_Shu <shuyue524@gmail.com>
2021-02-28 11:06:27 +0800

    将index.tsx转为index.js了！

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
-module.export = {
+const path = require("path");
+
+module.exports = {
+  mode: "development",
   entry: {
     index: "./lib/index.tsx",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/lib"), // 因为windows，mac系统的差别，对目录相对路径符号的不同，比如'/'，进行兼容
+    libraryTarget: "umd", // 「commonJS(Node)」+「amd(浏览器)」兼容， 最终导出定义的格式
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "awesome-typescript-loader",
+        use: "awesome-typescript-loader",
+        exclude: /node_modules/,
       },
     ],
   },
