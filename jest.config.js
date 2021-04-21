2dd0a2f12351baf6681d25c41ca1abccfc3ee6ef
Jason_Shu <shuyue524@gmail.com>
2021-04-21 23:23:04 +0800

    配置详细的测试覆盖率

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,10 @@ module.exports = {
   verbose: true,
   clearMocks: false,
   collectCoverage: false,
-  reporters: ["default"],
+  reporters: ["default", "jest-junit"],
+  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
   moduleDirectories: ["node_modules"],
   globals: {
